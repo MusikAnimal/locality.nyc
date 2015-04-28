e325cb829f36e1b554c723fda247441d6300799e
MusikAnimal <ziemba0123@gmail.com>
2015-04-28 00:37:46 -0400

    simple offline handling

--- javascripts/main.js
+++ javascripts/main.js
@@ -6,9 +6,11 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 ga('create', 'UA-55357845-1', 'auto');
 ga('send', 'pageview');
 
-var openedInfo = new google.maps.InfoWindow();
-var polyIsVisible = true;
-var isMobile = false;
+var openedInfo = new google.maps.InfoWindow(),
+    polyIsVisible = true,
+    isMobile = false,
+    isCordova = false,
+    stableConnection = true, reconnectTimeout, reconnectInterval = 3;
 
 $(document).ready((function() {
   if(!$("footer").is(":visible")) {
@@ -55,18 +57,43 @@ function addMainListeners() {
     }
   });
 
-  Offline.options = {
-    checks: {xhr: {url: 'connection-test'}},
-    checkOnLoad: false
-  };
-  Offline.on("down", function() {
-    $("#offline_notice").addClass("active");
-  });
-  Offline.on("up", function() {
-    $("#offline_notice").removeClass("active");
-  });
+  isCordova = typeof Connection !== "undefined" && navigator.connection;
 
   $("#offline_notice").on("click", function() {
-    alert("You are offline! locality.nyc uses Google Maps which may be cached on your device. Search functionality will not work while you are offline");
+    alert("You are offline! locality.nyc uses Google Maps which may be cached on your device, however search functionality will not work while you are offline.");
   });
+
+  setTimeout(checkNetwork, 5000);
+}
+
+function checkNetwork() {
+  if(isCordova) {
+    updateConnectivityState([Connection.UNKNOWN, Connection.CELL, Connection.NONE].indexOf(navigator.connection.type) === -1);
+  } else {
+    $.get("connection-test").done(function(data, xhr) {
+      updateConnectivityState(true);
+    }).fail(function() {
+      updateConnectivityState(false);
+    });
+  }
+}
+
+function updateConnectivityState(state) {
+  if(state) {
+    if(!stableConnection) {
+      stableConnection = true;
+      $("#offline_notice").removeClass("active");
+    }
+    setTimeout(checkNetwork, 5000);
+  } else {
+    if(stableConnection) {
+      stableConnection = false;
+      reconnectInterval = 0;
+      $("#offline_notice").addClass("active");
+    }
+    if(reconnectInterval < 5) {
+      reconnectInterval++;
+    }
+    setTimeout(checkNetwork, reconnectInterval * 1000);
+  }
 }
